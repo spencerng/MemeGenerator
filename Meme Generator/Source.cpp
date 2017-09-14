ecf4c3e0062f0f11ecb9487aaff4b4d6a416296e
Spencer Ng <sng1488@gmail.com>
2017-09-14 13:48:32 -0400

    init

---
+++ Meme Generator/Source.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+// qt or opencv : library to overlay text
+// subfolders w/ categories for images
+// windows api for gui
+// google natural language for profanity
+// imgur api to create albums
+
+int main() {
+
+
+
+}
